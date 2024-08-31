bca7f4f5de599a62e6bd743c43200f98c2f3f987
Your Name <you@example.com>
2024-08-31 12:56:15 +0300

    updated the loss function.

--- DepthLoss.py
+++ DepthLoss.py
@@ -2,13 +2,10 @@ import torch
 import torch.nn as nn
 
 class DepthLossF(nn.Module):
-    def __init__(self, C):
+    def __init__(self):
         super(DepthLossF, self).__init__()
-        self.C = C
     
     def forward(self, D1, D2):
-
-        diff = D1 - D2
-        loss = torch.mean(torch.relu(self.C - diff) + torch.relu(diff - 2*self.C))
-        
+        loss = torch.mean(((D1 - D2) >= 0).float())
+    
         return loss
