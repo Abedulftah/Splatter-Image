f82f74530e4563b68c4b3af946315f1ef0bc7ca0
Your Name <you@example.com>
2024-09-20 14:51:01 +0300

    A new loss function that take care of 0's which is e^-x.

--- DepthLoss.py
+++ DepthLoss.py
@@ -5,13 +5,8 @@ class DepthLossF(nn.Module):
     def __init__(self):
         super(DepthLossF, self).__init__()
     
-    def forward(self, D1, D2):
-        diff = D1 - D2
-        
-        mask1 = (diff > 0).float()
-        mask2 = (diff == 0).float() 
-        loss = torch.mean(diff * mask1 + 1e-3 * mask2)
-        
+    def forward(self, D_f):
+        loss = torch.mean(torch.exp(-D_f))
         return loss
 
 
